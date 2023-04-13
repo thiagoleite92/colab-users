2017b75bf5e919cc5f618ba1813bf5f037418e33
Thiago Leite <leite.tjs@gmail.com>
2023-04-13 17:54:39 -0300

    style: responsividade modal

--- src/pages/components/ModalCard/index.jsx
+++ src/pages/components/ModalCard/index.jsx
@@ -10,12 +10,15 @@ import {
 } from "react-icons/ai";
 import { BsFillTelephoneInboundFill } from "react-icons/bs";
 import ToggleLikeButton from "../ToggleLikeButton";
+import useWindowSize from "@/hooks/useWindowSize";
 
 export default function ModalCard() {
   const { userModal, setUserModal, openModal, setOpenModal } =
     useContext(AppContext);
 
   const { picture, location, cell, dob, email, nat, name, phone } = userModal;
+
+  const { width } = useWindowSize();
 
   const customStyles = {
     overlay: {
@@ -30,7 +33,7 @@ export default function ModalCard() {
       transform: "translate(-50%, -50%)",
       backgroundColor: "rgb(241,245,249)",
       width: "75%",
-      height: "50%",
+      height: width > 1023 ? "50%" : "75%",
     },
   };
 
@@ -51,14 +54,15 @@ export default function ModalCard() {
       contentLabel="Example Modal"
     >
       <section className="flex max-lg:flex-col gap-5">
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-3 items-center">
           <Image
             src={picture ? picture?.large : "/assets/images/notFoundUser.jpg"}
             alt="foto"
-            width={540}
-            height={480}
+            width={width > 1023 ? 540 : 410}
+            height={width > 1023 ? 480 : 305}
+            className="border-slate-300 border-2 p-1 rounded-sm"
           />
-          <div className="flex justify-between">
+          <div className="flex justify-around w-full">
             <button
               onClick={handleCloseModal}
               className="flex gap-2 hover:text-purple-800 text-purple-600"
@@ -80,7 +84,7 @@ export default function ModalCard() {
           <span>
             <strong>Data de Nascimento:</strong> {dob?.date} - {dob?.age} anos
           </span>
-          <div className="flex gap-4">
+          <div className="flex gap-4 max-md:flex-col">
             <span>
               <strong>Nacionalidade:</strong> {nat}
             </span>
@@ -100,7 +104,7 @@ export default function ModalCard() {
           </p>
           <div className="flex flex-col justify-center items-center gap-2">
             <strong>Entre em contato</strong>
-            <div className="flex gap-5">
+            <div className="flex gap-5 max-md:flex-col">
               <span className="contact-span">
                 <AiOutlineWhatsApp />
                 {cell}
